d56674c4d4e4f95d63da31da5834185fb9c39a42
YutoYasujima <apyfm.3.25.y.y@gmail.com>
2025-03-25 16:03:48 +0900

    fixed:Lintチェック修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -33,11 +33,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [ 800, 800 ]
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fill: [50, 50, "Center"]
+    process resize_to_fill: [ 50, 50, "Center" ]
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
